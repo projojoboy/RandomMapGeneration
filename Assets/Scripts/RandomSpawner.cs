09915da1662ff843127e1d01060f401213ea34bc
Projojoboy <22048@ma-web.nl>
2018-11-02 13:41:32 +0100

    Added Project

---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour {
+
+	[SerializeField] GameObject[] objects;
+    private GameObject spawnedTemplate;
+
+    private void Start()
+    {
+        int rand = Random.Range(0, objects.Length);
+        spawnedTemplate = Instantiate(objects[rand], transform.position, Quaternion.identity);
+        spawnedTemplate.transform.parent = gameObject.transform;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(spawnedTemplate);
+
+            int rand = Random.Range(0, objects.Length);
+            spawnedTemplate = Instantiate(objects[rand], transform.position, Quaternion.identity);
+        }
+    }
+}
